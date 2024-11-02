5646b92fadf12eb069434b62c5896fbdb00b74ae
Alex <kelheor23@gmail.com>
2024-11-02 10:11:38 +0300

    create simple sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install curl
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install clangd
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
