e5a86bf207dfe4198e6441f914a43ab8cdd51f69
Alex <kelheor23@gmail.com>
2024-11-02 12:01:13 +0300

    tiny fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install curl
 sudo apt-get install nodejs
 sudo apt-get install npm
-sudo apt-get install clangd
+sudo apt-get install cmake
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
